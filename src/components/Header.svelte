99bf2ba3d33e2785d789d31a01945d7adbd7d08c
Paranjay Singh <parnexcodes@gmail.com>
2024-01-06 16:45:54 +0530

    home page ✅

---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<div>
+  <div class="py-12 px-24">
+    <img src="/logo.svg" alt="" />
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Header from "../components/Header.svelte";
+</script>
+
+<div class="min-h-screen bg-[#FFFFFF]">
+  <Header />
+  <div class="flex justify-between px-24 py-16">
+    <div class="flex flex-col basis-2/4 gap-8">
+      <p class="text-black font-thin text-4xl">
+        <span class="font-bold text-[#0428C5]">Reviewing</span> and
+        <span class="font-bold text-[#0428C5]">Listing</span> job applications made
+        easy for you
+      </p>
+      <p class="text-xl text-gray-600">
+        Manage your numerous job refferals using refer me. <span
+          class="text-[#FB980D]">Free, Convenient</span
+        >
+        & <span class="text-[#FB980D]">Easy to Use.</span>
+      </p>
+      <div class="pt-16 flex">
+        <a
+          href="/dashboard"
+          class="text-white bg-[#0428C5] self-start pl-8 pr-8 pt-4 pb-4 rounded-full"
+          >Create a Job Opening</a
+        >
+      </div>
+    </div>
+    <img src="/mainLogo.svg" alt="" />
+  </div>
+</div>
